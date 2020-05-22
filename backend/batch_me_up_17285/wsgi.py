0775de894f84872810b8ed5a80bcb8735859200e
Crowdbotics <team@crowdbotics.com>
2020-05-22 18:26:06 +0000

    Update models

--- backend/batch_me_up_17285/wsgi.py
+++ backend/batch_me_up_17285/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batch_me_up_17285.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "batch_me_up_17285.settings")
 
 application = get_wsgi_application()
